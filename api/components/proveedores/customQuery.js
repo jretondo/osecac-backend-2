f71d614336ca14de671a221d78a12e7a243d8458
jretondo <jretondo90@gmail.com>
2021-10-19 15:21:27 -0300

    ix functions

--- api/components/proveedores/customQuery.js
+++ api/components/proveedores/customQuery.js
@@ -1,11 +1,11 @@
 const getListLimit = (table, page, busqueda, tipo) => {
     const desdePag = ((parseInt(page) - 1) * 10)
 
-    return ` SELECT * FROM ${table} WHERE raz_soc LIKE '%${busqueda}%' AND tipo_prov = '${tipo}' LIMIT ${desdePag}, 10 `
+    return ` SELECT * FROM ${table} WHERE (raz_soc LIKE '%${busqueda}%' OR localidad LIKE '%${busqueda}%' OR encargado LIKE '%${busqueda}%' OR email LIKE '%${busqueda}%') AND tipo_prov = '${tipo}' LIMIT ${desdePag}, 10 `
 }
 
 const getCantTotal = (table, busqueda, tipo) => {
-    return ` SELECT COUNT(*) AS CANT FROM ${table} WHERE raz_soc LIKE '%${busqueda}%' AND tipo_prov = '${tipo}' `
+    return ` SELECT COUNT(*) AS CANT FROM ${table} WHERE (raz_soc LIKE '%${busqueda}%' OR localidad LIKE '%${busqueda}%' OR encargado LIKE '%${busqueda}%' OR email LIKE '%${busqueda}%') AND tipo_prov = '${tipo}' `
 }
 module.exports = {
     getListLimit,
